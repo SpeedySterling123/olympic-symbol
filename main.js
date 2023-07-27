1f3a73b1ceec52600f402a81b6cfca1bf7bdb169
SpeedySterling123 <139649357+SpeedySterling123@users.noreply.github.com>
2023-07-27 08:55:21 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle=red;
+ctx.lineWidth = 2;
+ctx.arc(200,200,40,0,2* Math.PI);
+ctx.stroke();
+
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle=yellow;
+ctx.lineWidth = 2;
+ctx.arc(200,200,40,0,2* Math.PI);
+ctx.stroke();
+
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle=white;
+ctx.lineWidth = 2;
+ctx.arc(200,200,40,0,2* Math.PI);
+ctx.stroke();
+
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle= black;
+ctx.lineWidth = 2;
+ctx.arc(200,200,40,0,2* Math.PI);
+ctx.stroke();
+
+ctx= canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle= green;
+ctx.lineWidth = 2;
+ctx.arc(200,200,40,0,2* Math.PI);
+ctx.stroke();
